0e1a213e9a41037e60ea6a35d037972c9df7c1fe
Anshika Semwal <semwal.anshi.10@gmail.com>
2023-11-27 14:07:06 +0530

    pulled changes from remote

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from local repository");
         System.out.println("Hello from remote repoistory");
-        Systme.out.println("changed master branch in remote");
+        System.out.println("changed master branch in remote");
     }
 }
